db9ffa2b86ae63c71f6a68a6ec269d3dc6e4bb2b
qodirovshohijahon <tasaddux@gmail.com>
2023-02-01 01:07:15 +0500

    [feat:] tf files

---
+++ terraform/kubernetes.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+variable "host" {
+  type = string
+}
+
+# variable "client_certificate" {
+#   type = string
+# }
+
+# variable "client_key" {
+#   type = string
+# }
+
+variable "cluster_ca_certificate" {
+  type = string
+}
+variable "cluster_name" {
+  type = string
+}
+
+
+# provider "kubernetes" {
+#   host = var.host
+
+#   client_certificate     = base64decode(var.client_certificate)
+#   client_key             = base64decode(var.client_key)
+#   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+# }
+
+provider "kubernetes" {
+  host = var.host
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", var.cluster_name]
+  }
+}
