6e2a9e1555987c2efee1d440041326eae63e1c3e
qodirovshohijahon <tasaddux@gmail.com>
2023-02-01 19:11:10 +0500

    fix

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "Current Region"
+}
+
+variable "access_key" {
+  type        = string
+  default     = "AKIAUEE33YYWZTRVBHLJ"
+  description = "Access Key"
+}
+
+variable "secret_key" {
+  type        = string
+  default     = "f/E1KDTLD4iOkC0VorJH2Kz7G9/pC2sOo5um6pNK"
+  description = "Secret Access Key"
+}
